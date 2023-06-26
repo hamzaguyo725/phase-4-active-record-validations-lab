1d510c6afb99b4b657e3c8bcdbf48c941128547c
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-06-26 11:58:12 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    CATEGORIES = ['Fiction','Non-Fiction']
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: {in: CATEGORIES }
+    validate :title_must_have
+
+    def title_must_have
+        Phrase= ["Won't Believe","Secret","Top[number]","Guess"]
+
+        unless Phrase.any? {|phrase| title.include?(phrase) 
+        error.add{error: "title should include phrase"}}
+    
+    
+    end
+
+    
 end
